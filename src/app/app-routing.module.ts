75ce080f75a575da36fe4b919b296bc60b1261a6
coryd343 <fizzy.gutsnipe@outlook.com>
2022-11-23 02:11:39 -0800

    Added stubs and routing for main pages and nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './core/landing/landing.component';
+import { BlogComponent } from './core/blog/blog.component';
+import { AboutComponent } from './core/about/about.component';
+import { GalleryComponent } from './core/gallery/gallery.component';
+import { StoreComponent } from './core/store/store.component';
 
 const routes: Routes = [
-  {path: "", component: LandingComponent}
+  { path: "", component: LandingComponent },
+  { path: "blog", component: BlogComponent },
+  { path: "about", component: AboutComponent },
+  { path: "gallery", component: GalleryComponent },
+  { path: "store", component: StoreComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 
 import { LandingComponent } from './core/landing/landing.component';
+import { NavComponent } from './masthead/nav/nav.component';
+import { BlogComponent } from './core/blog/blog.component';
+import { GalleryComponent } from './core/gallery/gallery.component';
+import { StoreComponent } from './core/store/store.component';
+import { AboutComponent } from './core/about/about.component';
+import { MastheadComponent } from './masthead/masthead.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    NavComponent,
+    BlogComponent,
+    GalleryComponent,
+    StoreComponent,
+    AboutComponent,
+    MastheadComponent
   ],
   imports: [
     BrowserModule,
